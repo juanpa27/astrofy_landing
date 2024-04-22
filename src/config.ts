3e643de0a0413845f9f886388de915ad980dc5e2
jbellenzier <jbellenzier@gmail.com>
2024-04-22 14:04:28 -0400

    cambio en botones

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
+export const SITE_TITLE = 'Dra. Celeste Cuevas | Psiquiatra';
+export const SITE_DESCRIPTION = 'Psiquiatra especializada. Tratamiento integral para trastornos de ansiedad, depresión y más. ¡Recupera tu bienestar emocional hoy mismo!';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
+
